b624489848822876b9205d2543ab8c32e9e06b81
Artemus Hayden <grayson2005hayden@gmail.com>
2023-08-03 12:42:36 -0500

    Finish add, sub, div, mult function logic

--- script.js
+++ script.js
@@ -5,36 +5,36 @@ const NUMBER_OF_TERMS_EVALUATED = 2;
 let termNumber=""
 let add, subtract, divison, multiply, operator = false;
 
-
-
 function addFun(total, number) {
     total += number
     return total
 }
 
-function subtractFun(){return "sub"}
-
-function divisonFun(){return "div"}
-
-function multiplyFun(total, number){
-    total = 1;
-    total * number
-    return total;
+function subtractFun(array){
+    return (array[NUMBER_OF_TERMS_EVALUATED -2] - array[NUMBER_OF_TERMS_EVALUATED - 1])
 }
 
+function divisonFun(array){
+    return (array[NUMBER_OF_TERMS_EVALUATED -2] / array[NUMBER_OF_TERMS_EVALUATED - 1])
+}
 
-
+function multiplyFun(array){
+    const intial = 1;
+    const sumWithInitial = array.reduce(
+    (accumulator, currentValue) => accumulator * currentValue, intial)
+  return sumWithInitial
+}
 
 function equalClicked() {
     numbers.push(Number(termNumber));
     if (add === true){
         console.log(numbers.reduce(addFun)); 
     } else if (subtract === true){
-        console.log(numbers.reduce(subtractFun))
+        console.log(subtractFun(numbers))
     } else if (divison === true){
-        console.log(numbers.reduce(divisonFun))
+        console.log(divisonFun(numbers))
     } else {
-        console.log(numbers.reduce(multiplyFun))
+        console.log(multiplyFun(numbers))
     }
 }
 
@@ -60,71 +60,3 @@ function getNumber(value) {
     console.log(termNumber)
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// function getNumber(value) {
-//     if (operator === true)
-//         if (numbers.length < NUMBER_OF_TERMS_EVALUATED) {
-//             numbers.push(Number(termNumber));
-//             console.log(numbers)
-
-//             termNumber += value
-//             console.log(termNumber)
-
-
-//         } else {
-//             console.log(numbers.reduce(add));
-//             numbers.push(numbers.reduce(add));
-//             numbers = []
-//             console.log(numbers)
-//     } else {
-//         termNumber += value
-//         console.log(termNumber)
-//     }
-// }
-
-// function operatorClicked() {
-//     operator = true;
-// }
-
-// function add(total, number) {
-//     total += number
-//     return total
-// }
-
-
-// let numbers = []
-// const NUMBER_OF_TERMS_EVALUATED = 2;
-// let total = 0
-
-// function getNumbers(value) {
-//     numbers.push(value);
-//     console.log(numbers)
-// }
-// function add(total, number) {
-//     total += number
-//     return total
-// }
-
-
-// function getNumbers(value) {
-    // if (numbers.length < NUMBER_OF_TERMS_EVALUATED) {
-    //     numbers.push(Number(value));
-    //     console.log(numbers)
-    // } else {
-    //     console.log(numbers.reduce(add));
-    //     numbers.push(numbers.reduce(add));
-    //     numbers = []
-    //     console.log(numbers)
-    // }
-// }
